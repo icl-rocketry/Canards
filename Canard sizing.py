1f3f27da34fb7b1e575c7339113a25574180433e
daniel-dyon <117684164+daniel-dyon@users.noreply.github.com>
2023-12-05 20:01:37 +0000

    Update Canard sizing.py
    no

--- Canard sizing.py
+++ Canard sizing.py
@@ -3,47 +3,56 @@ import os
 import matplotlib.pyplot as plt
 os.system("cls")
 
-time = 2 #time canards are active
-initialRollRate = np.radians(1000) #degrees per second
+time = 5 #time canards are active
+initialRollRate = np.radians(500) #degrees per second
 Ixx = 0.2487 #kgm^2
 numberOfCanards = 3
 dt = 0.01 #choose time step
-vy0 =160 #initial velocity when canards are actuated
-vyt = 0 #final velocity when canards are actuated
+vy0 =100 #initial velocity when canards are actuated
+vyt = 10 #final velocity when canards are actuated
 vy = np.linspace(vy0,vyt,round(time/dt)) #generate vertical velocity array
 trange = np.linspace(0,time,round(time/dt)) #generate time array
 
 
 omega = [initialRollRate] #initialise roll rate
-Minit = 1 #initialise value of initial moment
+Minit = 0.1 #initialise value of initial moment
 
 while abs(omega[-1])>1: #loop to find intial required moment
     k = Minit/(vy0**2) #M=k*v^2
     M = []
     omega = [initialRollRate] #reset roll rate
+    beta = [np.arctan(initialRollRate*0.13/vy0)]
 
     for i in range(round(time/dt)-1):
         M.append(k*vy[i]**2) #store moment value
         omegai = omega[i] - (M[i]/Ixx)*dt #calculate next value of roll rate
         omega.append(omegai)
+        beta.append(np.arctan(omegai*0.13/vy[i]))
+
     
     Minit = Minit + float(0.1)
 
 print("Initial value of torque =",Minit)
 
+#plt.plot(trange,np.degrees(beta))
+#plt.show()
+
 #Canard design
 
 MDDeff = 0.7 #mach drag divergence of aerofoil
-MDD = 0.82 #Max mach number of flight
-Cl = 1.03 #Cl of aerofoil at 10degrees AOA
-Cr = 0.14; #root chord
-dxEst = 0.16 #guess for value of moment arm - to be iterated
+MDD = 0.78 #Max mach number of flight
+Cl = 0.2 #Cl of aerofoil at 10degrees AOA actually 2 degrees
+Cr = 0.1 #root chord
+dxEst = 0.12 #guess for value of moment arm - to be iterated
 phi25 = np.arccos((MDDeff/MDD)**(1)) #calculate quarter chord sweep angle
 print("Quarter chord sweep angle",np.degrees(phi25))
-Area = (2*Minit/3)/(Cl*np.cos(phi25) * vy[0]**2 * 1.1 * dxEst) #from lift equation and modified cl from sweep eq
+Cl3d =Cl*np.cos(phi25)**2
+Area = (2*Minit/3)/( Cl3d * vy[0]**2 * 1.1 * dxEst) #from lift equation and modified cl from sweep eq
 Ct = np.sqrt(Cr**2-8*Area*np.tan(phi25)/3) #find tip chord given sweep angle and area
+print("Adjusted CL =", Cl3d)
 print("Tip chord =",Ct)
 b = 2*Area/(Cr+Ct) #find span of single canard
+print("Sref=",Area)
 print("Canard span =",b)
 
 x = [0,0,b,b,0]
